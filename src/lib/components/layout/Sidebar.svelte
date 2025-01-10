91981162b23e33104669a3f9c1e054d5f49fe9a1
profmancusoa <profmancusoa@gmail.com>
2025-01-10 17:26:41 +0100

    - removed badge from sidebar - updated acme logo

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -12,19 +12,14 @@
 	import {
 		AngleDownOutline,
 		AngleUpOutline,
-		ClipboardListSolid,
-		CogSolid,
 		GithubSolid,
-		LifeSaverSolid,
-		ChartPieOutline,
-		TableColumnSolid,
-		BugSolid,
+		FileDocSolid,
+		FileLinesSolid,
 		EnvelopeOpenSolid,
 		RocketSolid,
 		GiftBoxSolid,
 		GridSolid,
-		FileDocSolid,
-		FileLinesSolid
+		BugSolid
 	} from 'flowbite-svelte-icons';
 
 	import Badge from './Badge.svelte';
@@ -37,9 +32,11 @@
 
 	let iconClass =
 		'flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white';
+	// let itemClass =
+	// 	'flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700';
+	let itemClass =
+		'flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700';
 	let groupClass = 'pt-2 space-y-2';
-	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
-	let subSpanClass = 'flex-1 px-8 ms-3 whitespace-nowrap';
 
 	$: mainSidebarUrl = $page.url.pathname;
 	let activeMainSidebar: string;
@@ -58,12 +55,6 @@
 			name: 'Menu 2', 
 			icon: RocketSolid, 
 			href: '/pageMenu2',  
-			badge: {
-				type: 'text',
-				value: 'Advanced',
-				bgColor: 'bg-orange-200',
-				fgColor: 'text-orange-900'
-			}
 		},
 		{
 			name: 'Menu 3',
@@ -85,12 +76,6 @@
 			name: 'Menu 5', 
 			icon: BugSolid, 
 			href: '/settings',
-			badge: {
-				type: 'number',
-				value: '5',
-				bgColor: 'bg-green-200',
-				fgColor: 'text-green-800'
-			}
 		}
 	];
 
@@ -114,59 +99,62 @@
 	let dropdowns = Object.fromEntries(Object.keys(posts).map((x) => [x, false]));
 </script>
 
-<Sidebar {mainSidebarUrl}
+<Sidebar
 	class={drawerHidden ? 'hidden' : ''}
 	activeUrl={mainSidebarUrl}
 	activeClass="bg-gray-100 dark:bg-gray-700"
 	asideClass="fixed inset-0 z-30 flex-none h-full w-64 lg:h-auto border-e border-gray-200 dark:border-gray-600 lg:overflow-y-visible lg:pt-16 lg:block"
-	>
+>
 	<h4 class="sr-only">Main menu</h4>
-  <SidebarWrapper 
-  	divClass="overflow-y-auto px-3 pt-20 lg:pt-5 h-full bg-white scrolling-touch max-w-2xs lg:h-[calc(100vh-4rem)] lg:block dark:bg-gray-800 lg:me-0 lg:sticky top-2"
-  >
-  	<nav class="divide-y divide-gray-200 dark:divide-gray-700">
-		<SidebarGroup ulClass={groupClass} class="mb-3">
-			{#each posts as { name, icon, children, href, badge } (name)}
-				{#if children}
-					<SidebarDropdownWrapper bind:isOpen={dropdowns[name]} label={name} class="pr-3">
-						<AngleDownOutline slot="arrowdown" strokeWidth="3.3" size="sm" />
-						<AngleUpOutline slot="arrowup" strokeWidth="3.3" size="sm" />
-						<svelte:component this={icon} slot="icon" class={iconClass} />
+	<SidebarWrapper
+		divClass="overflow-y-auto px-3 pt-20 lg:pt-5 h-full bg-white scrolling-touch max-w-2xs lg:h-[calc(100vh-4rem)] lg:block dark:bg-gray-800 lg:me-0 lg:sticky top-2"
+	>
+		<nav class="divide-y divide-gray-200 dark:divide-gray-700">
+			<SidebarGroup ulClass={groupClass} class="mb-3">
+				{#each posts as { name, icon, children, href, badge } (name)}
+					{#if children}
+						<SidebarDropdownWrapper bind:isOpen={dropdowns[name]} label={name} class="pr-3">
+							<AngleDownOutline slot="arrowdown" strokeWidth="3.3" size="sm" />
+							<AngleUpOutline slot="arrowup" strokeWidth="3.3" size="sm" />
+							<svelte:component this={icon} slot="icon" class={iconClass} />
 
-						{#each Object.entries(children) as [title, href]}
-							<SidebarItem
-								label={title}
-								{href}
-								spanClass={subSpanClass}
-							/>
-						{/each}
-					</SidebarDropdownWrapper>
-				{:else}
+							{#each Object.entries(children) as [title, href]}
+								<SidebarItem
+									label={title}
+									{href}
+									spanClass="ml-9"
+									class={itemClass}
+								/>
+							{/each}
+						</SidebarDropdownWrapper>
+					{:else}
+						<SidebarItem
+							label={name}
+							{href}
+							spanClass="ml-3"
+							class={itemClass}
+						>
+							<svelte:component this={icon} slot="icon" class={iconClass} />
+							<Badge {...badge} slot="subtext" />
+						</SidebarItem>
+					{/if}
+				{/each}
+			</SidebarGroup>
+			<SidebarGroup ulClass={groupClass}>
+				{#each links as { label, href, icon } (label)}
 					<SidebarItem
-						label={name}
+						{label}
 						{href}
-						{spanClass}
+						spanClass="ml-3"
+						class={itemClass}
+						target="_blank"
 					>
-						<svelte:component this={icon} slot="icon" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" /> 
-						<Badge {...badge} slot="subtext" />
+						<svelte:component this={icon} slot="icon" class={iconClass} />
 					</SidebarItem>
-				{/if}
-			{/each}
-		</SidebarGroup>
-		<SidebarGroup ulClass={groupClass}>
-			{#each links as { label, href, icon } (label)}
-				<SidebarItem
-					{label}
-					{href}
-					{spanClass}
-					target="_blank"
-				>
-					<svelte:component this={icon} slot="icon" class={iconClass} />
-				</SidebarItem>			
-			{/each}
-		</SidebarGroup>
-	</nav>
-  </SidebarWrapper>
+				{/each}
+			</SidebarGroup>
+		</nav>
+	</SidebarWrapper>
 </Sidebar>
 
 <div
@@ -176,3 +164,4 @@
 	on:keydown={closeDrawer}
 	role="presentation"
 ></div>
+
